d103e361067440cf30eef590e09d2b4fc9022531
Jae Cho <jc960116@gmail.com>
2022-04-30 16:27:25 -0700

    update resume pdf

--- src/components/header/CT.jsx
+++ src/components/header/CT.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../../assets/Jae_Cho_Resume2022.pdf";
+import resume from "../../assets/JaeCho_SoftwareEngineer.pdf";
 const CT = () => {
   return (
     <div className="cta">
